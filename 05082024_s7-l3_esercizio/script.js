ae60ae8b64de62931dfdb56315f8f15efccc426d
bertojr <1020bertone@gmail.com>
2024-05-08 16:06:13 +0200

    creata funzione per generare libri all'interno dll'HTML

---
+++ 05082024_s7-l3_esercizio/script.js
@@ -0,0 +1,42 @@
+const getLibri = function () {
+  fetch("https://striveschool-api.herokuapp.com/books")
+    .then((response) => {
+      if (response.ok) {
+        console.log("Arrivato");
+        return response.json();
+      } else {
+        throw new Error("La risposta del server è stata negativa");
+      }
+    })
+    .then((arrayLibri) => {
+      console.log(arrayLibri);
+      generaLibriHtml(arrayLibri);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const generaLibriHtml = function (libri) {
+  const rigaLibri = document.getElementsByClassName("row")[0];
+  console.log(rigaLibri);
+  libri.forEach((libro) => {
+    const newCol = document.createElement("div");
+    newCol.classList.add("col", "mb-4");
+    newCol.innerHTML = `
+        <div class="card">
+            <img src="${libro.img}" class="card-img-top" alt="${libro.title}">
+            <div class="card-body">
+                <h5 class="card-title">${libro.title}</h5>
+                <h6 class="card-text text-end py-3">Prezzo: ${libro.price} €</h6>
+                <a href="#" class="btn btn-danger">SCARTA</a>
+            </div>
+        </div>
+        `;
+    rigaLibri.appendChild(newCol);
+  });
+};
+
+const rimuoviLibriHtml = function () {};
+
+getLibri();
