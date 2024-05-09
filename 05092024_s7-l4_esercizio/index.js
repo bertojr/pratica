eb844aefaabb086e283888cadd14a7a2c6d77e3b
bertojr <1020bertone@gmail.com>
2024-05-09 18:19:37 +0200

    genera immagini con l'input di ricerca

--- 05092024_s7-l4_esercizio/index.js
+++ 05092024_s7-l4_esercizio/index.js
@@ -1,63 +1,21 @@
 // costanti per comporre l'URL dell'api
-/*
+
 const apiKey = "ZHG9vuhefNwTRLLJmqmS1iVsoIT7TaJdukzvfMKrrlsNROuzSuqUEaKI";
 const apiUrl = "https://api.pexels.com/v1/";
-const query = "cats";
-const url = apiUrl + "search?query=" + query;*/
+//let query;
+//const url = apiUrl + "search?query=" + query;
 
 // prendo il riferimento al bottone load Images
 const loadImages = document.getElementById("load-images");
 
 // prendo il riferimento al bottone load secondary images
-const loadSecondaryimages = document.getElementById("load-secondary-image");
+const loadSecondaryimages = document.getElementById("load-secondary-images");
 
-const generateImages = function (arrayImages) {
-  const row = document.getElementById("row-card");
-  console.log(row);
-  arrayImages.forEach((image) => {
-    console.log(image);
-    const newCol = document.createElement("div");
-    newCol.classList.add("col");
-    newCol.innerHTML = `
-              <div class="card mb-4 shadow-sm">
-                <img
-                  src="${image.src.medium}"
-                  class="bd-placeholder-img card-img-top"
-                />
-                <div class="card-body">
-                  <h5 class="card-title">Lorem Ipsum</h5>
-                  <p class="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div class="btn-group">
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        View
-                      </button>
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        Edit
-                      </button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            `;
-    row.appendChild(newCol);
-  });
-};
-const getCats = function () {
-  fetch(url, {
+// prenod il riferimento al bottone search
+const search = document.getElementById("btn-search");
+
+const getImagesApi = function (query) {
+  fetch(apiUrl + "search?query=" + query, {
     headers: {
       Authorization: apiKey,
     },
@@ -72,14 +30,76 @@ const getCats = function () {
     })
     .then((arr) => {
       console.log(arr.photos);
-
-      loadImages.addEventListener("click", function () {
-        generateImages(arr.photos);
-      });
+      generateImages(arr.photos);
     })
     .catch((err) => {
       console.log("ERRORE", err);
     });
 };
 
-getCats();
+const generateImages = function (arrayImages) {
+  const row = document.getElementById("row-card");
+
+  row.innerHTML = "";
+  console.log(row);
+  arrayImages.forEach((image) => {
+    console.log(image);
+    const newCol = document.createElement("div");
+    newCol.classList.add("col");
+    newCol.innerHTML = `
+                <div class="card mb-4 shadow-sm">
+                  <img
+                    src="${image.src.original}"
+                    class="bd-placeholder-img card-img-top"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">Lorem Ipsum</h5>
+                    <p class="card-text">
+                      This is a wider card with supporting text below as a natural
+                      lead-in to additional content. This content is a little bit
+                      longer.
+                    </p>
+                    <div
+                      class="d-flex justify-content-between align-items-center"
+                    >
+                      <div class="btn-group">
+                        <button
+                          type="button"
+                          class="btn btn-sm btn-outline-secondary"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          class="btn btn-sm btn-outline-secondary"
+                          onclick="removeImages(this)"
+                        >
+                          Hide
+                        </button>
+                      </div>
+                      <small class="text-muted">${image.id}</small>
+                    </div>
+                  </div>
+                </div>
+              `;
+    row.appendChild(newCol);
+  });
+};
+
+const removeImages = function (btn) {
+  const card = btn.closest(".col");
+  console.log(card);
+  card.classList.add("d-none");
+};
+
+getImagesApi("stadium");
+loadImages.addEventListener("click", function () {
+  getImagesApi("ball");
+});
+loadSecondaryimages.addEventListener("click", function () {
+  getImagesApi("spider-man");
+});
+search.addEventListener("click", function () {
+  const searchInput = document.getElementById("search-custom");
+  getImagesApi(searchInput.value);
+});
