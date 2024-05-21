af7a1cda46010d87fee2ddd99a4a7a366004fced
bertojr <1020bertone@gmail.com>
2024-05-21 15:19:41 +0200

    .

--- 05212024_s8-l2_esercizio/src/app/app.component.ts
+++ 05212024_s8-l2_esercizio/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = '05212024_s8-l2_esercizio';
+  title = 'Esercizio';
 }
--- 05212024_s8-l2_esercizio/src/app/app.module.ts
+++ 05212024_s8-l2_esercizio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Component1Component } from './components/component1/component1.component';
+import { Component2Component } from './components/component2/component2.component';
+import { Component3Component } from './components/component3/component3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component
   ],
   imports: [
     BrowserModule,
