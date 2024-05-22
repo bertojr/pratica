8639c7c984b7a8e273f14feb77c573a3c521aa93
bertojr <1020bertone@gmail.com>
2024-05-22 16:43:56 +0200

    .

--- 05222024_s8-l3_esercizio/src/app/app.component.ts
+++ 05222024_s8-l3_esercizio/src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = '05222024_s8-l3_esercizio';
-}
+export class AppComponent {}
--- 05222024_s8-l3_esercizio/src/app/components/home/home.component.ts
+++ 05222024_s8-l3_esercizio/src/app/components/home/home.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { iPost } from '../../models/post';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  urlJson = '../../../assets/db.json';
 
+  postArr: iPost[] = [];
+  fourPostsRandom: number[] = [];
+
+  ngOnInit() {
+    fetch(this.urlJson)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        this.postArr = response.posts;
+        console.log(this.postArr);
+        this.postsRandom();
+      });
+  }
+
+  postsRandom() {
+    for (let i = 0; i < 3; i++) {
+      this.fourPostsRandom.push(i);
+    }
+    /*
+    for (let i = 0; i < 3; i++) {
+      const randomIndex = Math.floor(Math.random() * this.postArr.length);
+    }
+    */
+  }
 }
