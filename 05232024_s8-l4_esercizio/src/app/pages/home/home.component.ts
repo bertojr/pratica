7086b64f688227e08bdb77315006dd0b927e7058
bertojr <1020bertone@gmail.com>
2024-05-23 16:42:41 +0200

    .

--- 05232024_s8-l4_esercizio/src/app/pages/home/home.component.ts
+++ 05232024_s8-l4_esercizio/src/app/pages/home/home.component.ts
@@ -10,12 +10,22 @@ import { iPost } from '../../models/post';
 export class HomeComponent {
   url: string = '../../../assets/db.json';
   postArr: iPost[] = [];
+  fourPostsRandom: iPost[] = [];
   ngOnInit() {
     fetch(this.url)
       .then((response) => response.json())
       .then((data) => {
         this.postArr = data.posts;
         console.log(this.postArr);
+        this.generatePostsRandom();
       });
+  }
+
+  generatePostsRandom() {
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * this.postArr.length);
+      this.fourPostsRandom.push(this.postArr[randomIndex]);
+    }
+    console.log(this.fourPostsRandom);
   }
 }
