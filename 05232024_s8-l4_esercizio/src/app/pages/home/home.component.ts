8721c2b69ddddc4de40e9dd48887fc36a6cdffa9
bertojr <1020bertone@gmail.com>
2024-05-23 16:26:13 +0200

    .

--- 05232024_s8-l4_esercizio/src/app/pages/home/home.component.ts
+++ 05232024_s8-l4_esercizio/src/app/pages/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { PostsService } from '../../posts.service';
+import { iPost } from '../../models/post';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  url: string = '../../../assets/db.json';
+  postArr: iPost[] = [];
+  ngOnInit() {
+    fetch(this.url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.postArr = data.posts;
+        console.log(this.postArr);
+      });
+  }
 }
