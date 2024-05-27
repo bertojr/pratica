a84113e9cb7263adb3a02be2eaa0af40b3e9e376
bertojr <1020bertone@gmail.com>
2024-05-27 15:02:53 +0200

    first commit

---
+++ 05272024_s10-l1_esercizio/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './main-components/navbar/navbar.component';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ActivePostsComponent } from './pages/active-posts/active-posts.component';
+import { InactivePostsComponent } from './pages/inactive-posts/inactive-posts.component';
+import { PostDetailComponent } from './pages/post-detail/post-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
+    PostDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
