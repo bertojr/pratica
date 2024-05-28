5544a771028d7f773a74837fc0d6acc691cee10c
bertojr <1020bertone@gmail.com>
2024-05-28 14:36:24 +0200

    first commit

---
+++ 05282024_s10-l2_esercizio/src/app/pages/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { PostsService } from '../../posts.service';
+import { iPost } from '../../models/post';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  postsArr: iPost[] = [];
+  fourPostsRandom: iPost[] = [];
+  formVisible: boolean = false;
+  constructor(private postsSvc: PostsService) {}
+
+  ngOnInit() {
+    this.postsArr = this.postsSvc.postsArr;
+    console.log(this.postsArr);
+    this.generatePostsRandom();
+  }
+
+  generatePostsRandom() {
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * this.postsArr.length);
+      this.fourPostsRandom.push(this.postsArr[randomIndex]);
+    }
+    console.log(this.fourPostsRandom);
+  }
+
+  toggleForm() {
+    this.formVisible = !this.formVisible;
+  }
+}
---
+++ 05282024_s10-l2_esercizio/src/app/pages/inactive-posts/inactive-posts.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { iPost } from '../../models/post';
+import { PostsService } from '../../posts.service';
+
+@Component({
+  selector: 'app-inactive-posts',
+  templateUrl: './inactive-posts.component.html',
+  styleUrl: './inactive-posts.component.scss',
+})
+export class InactivePostsComponent {
+  postInactiveArr: iPost[] = [];
+  constructor(private postsSvc: PostsService) {}
+
+  ngOnInit() {
+    this.postInactiveArr = this.postsSvc.getPostInactive();
+  }
+}
