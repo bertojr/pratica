06c6d0bdd3e86690d96f4258af2c92dec14c8f0b
bertojr <1020bertone@gmail.com>
2024-05-29 14:54:20 +0200

    .

--- 05292024_s10-l3_esercizio/src/app/app.module.ts
+++ 05292024_s10-l3_esercizio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { HeaderComponent } from './main-components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FavoritesComponent,
+    CartComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
