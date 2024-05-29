7bd23bf4ff03b515788d1f2b20b24f0a30fadd61
bertojr <1020bertone@gmail.com>
2024-05-29 14:29:18 +0200

    first commit

---
+++ 05292024_s10-l3_esercizio/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = '05292024_s10-l3_esercizio';
+}
