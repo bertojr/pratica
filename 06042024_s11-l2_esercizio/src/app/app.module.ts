78b406511044728497fa6fb04f14f3fa1d12a887
bertojr <1020bertone@gmail.com>
2024-06-04 14:36:29 +0200

    first commit

---
+++ 06042024_s11-l2_esercizio/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReactiveFormComponent,
+    TemplateDrivenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
