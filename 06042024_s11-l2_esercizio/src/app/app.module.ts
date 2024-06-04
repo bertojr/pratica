66d9f6a81d341047d179acfd8e0fb9d1853e4e9e
bertojr <1020bertone@gmail.com>
2024-06-04 16:44:05 +0200

    .

--- 06042024_s11-l2_esercizio/src/app/app.module.ts
+++ 06042024_s11-l2_esercizio/src/app/app.module.ts
@@ -6,19 +6,12 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ReactiveFormComponent,
-    TemplateDrivenComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  declarations: [AppComponent, ReactiveFormComponent, TemplateDrivenComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- 06042024_s11-l2_esercizio/src/app/reactive-form/reactive-form.component.ts
+++ 06042024_s11-l2_esercizio/src/app/reactive-form/reactive-form.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrl: './reactive-form.component.scss'
+  styleUrl: './reactive-form.component.scss',
 })
 export class ReactiveFormComponent {
+  form!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      nome: this.fb.control('Paulo', Validators.required),
+      cognome: this.fb.control('Dybala', Validators.required),
+      authData: this.fb.group({
+        password: this.fb.control(1234, [
+          Validators.required,
+          Validators.minLength(6),
+          this.prova(),
+        ]),
+        confermaPassword: this.fb.control(1234, [
+          Validators.required,
+          Validators.minLength(6),
+        ]),
+        username: this.fb.control('pau', Validators.required),
+      }),
+      genere: this.fb.control('altro', Validators.required),
+      file: this.fb.control(null),
+      biografia: this.fb.control(null),
+    });
+  }
+  invia() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      console.log('Form non valido');
+    }
+  }
+
+  prova(): ValidationErrors | null {
+    return { prova: true };
+  }
 }
