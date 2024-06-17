ddd0a76aea66235efaeb880cc89d8031e5db21df
bertojr <78014252+bertojr@users.noreply.github.com>
2024-06-17 16:42:54 +0200

    Add files via upload

---
+++ 06172024_s01-l1_esercizio/Dipendente.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05172024_s01_l1_esercizio
+{
+    internal class Dipendente
+    {
+        string name;
+        string lastName;
+        int stipendio;
+        
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public int Stipendio
+        {
+            get
+            {
+                return stipendio;
+            }
+            set
+            {
+                stipendio = value;
+            }
+        }
+
+        public void Description()
+        {
+            Console.WriteLine(name + " " + lastName + " - " + stipendio+" EURO al mese");
+        }
+    }
+}
