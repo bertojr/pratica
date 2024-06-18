185411ec90580f399e4c39219108010bceeae7e3
bertojr <78014252+bertojr@users.noreply.github.com>
2024-06-18 15:22:13 +0200

    Add files via upload

---
+++ 06182024_s1-l2_esercizio/Persona.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06182024_s1_l2_esercizio
+{
+    internal class Persona
+    {
+        string nome;
+        string cognome;
+        int eta;
+
+        public Persona(string nome, string cognome, int eta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.eta = eta;
+        }
+
+        public string Nome 
+        { 
+            get 
+            { 
+                return nome;  
+            } 
+            set 
+            { 
+                nome = value; 
+            }
+        }
+        public string Cognome
+        {
+            get
+            {
+                return cognome;
+            }
+            set
+            {
+                cognome = value;
+            }
+        }
+        public int Eta
+        {
+            get
+            {
+                return eta;
+            }
+            set
+            {
+                eta = value;
+            }
+        }
+
+        public string Dettagli()
+        {
+            return nome+" "+cognome+" - "+eta+" anni";
+        }
+
+
+    }
+}
