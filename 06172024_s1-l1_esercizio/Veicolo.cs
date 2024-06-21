49e009babf60d10131ce2358804514d227a1c504
bertojr <1020bertone@gmail.com>
2024-06-21 12:28:22 +0200

    rinomina cartella

---
+++ 06172024_s1-l1_esercizio/Veicolo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05172024_s01_l1_esercizio
+{
+    internal class Veicolo
+    {
+        string marca;
+        string modello;
+        string colore;
+        int velocitaMax;
+
+        public string Marca
+        {
+            get
+            {
+                return marca;
+            }
+            set
+            {
+                marca = value;
+            }
+        }
+        public string Modello
+        {
+            get
+            {
+                return modello;
+            }
+            set
+            {
+                modello = value;
+            }
+        }
+        public string Colore
+        {
+            get
+            {
+                return colore;
+            }
+            set
+            {
+                colore = value;
+            }
+        }
+        public int VelocitaMax
+        {
+            get
+            {
+                return velocitaMax;
+            }
+            set
+            {
+                velocitaMax = value;
+            }
+        }
+
+        public void Description()
+        {
+            Console.WriteLine("Marca: "+marca+" - Modello: "+modello+" - Velocià massima: "+velocitaMax);
+        }
+    }
+}
